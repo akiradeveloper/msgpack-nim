33024b5c52ccb417f139b7d3f03b2ebd51a7baca
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-23 08:54:23 +0900

    add cycle test
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- msgpack.nim
+++ msgpack.nim
@@ -5,5 +5,30 @@
 
 # ------------------------------------------------------------------------------
 
+type
+  MsgKind = enum
+    mkNil
+  Msg = object
+    case kind: MsgKind
+    of mkNil: nil
+
+type Bin = seq[uint8]
+
+proc Nil(): Msg =
+  Msg(kind: mkNil)
+
+proc pack(msg: Msg): Bin =
+  @[]
+  
+proc unpack(s: Bin): Msg =
+  Nil()
+
+# ------------------------------------------------------------------------------
+
+template t(msg: Msg) =
+  let packed = pack(msg)
+  let unpacked = unpack(packed)
+  assert($expr(Nil()) == $expr(unpacked))
+
 when isMainModule:
-  discard
+  t(Nil())
