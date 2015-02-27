b99ee0c2ee0b68e9566b4780df680a9bc9ced7aa
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-27 17:39:41 +0900

    add test for fixmap
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- GenTests.hs
+++ GenTests.hs
@@ -51,7 +51,7 @@ msgShow (MsgFixStr s) = "FixStr(" ++ show s ++ ")"
 msgShow (MsgStr8 s) = "Str8(" ++ show s ++ ")"
 msgShow (MsgStr16 s) = "Str16(" ++ show s ++ ")"
 msgShow (MsgStr32 s) = "Str32(" ++ show s ++ ")"
-msgShow (MsgFixMap xs) = "Fixmap(@[" ++ mapShow xs ++ "])"
+msgShow (MsgFixMap xs) = "FixMap(@[" ++ mapShow xs ++ "])"
 msgShow (MsgFloat32 n) = "Float32(" ++ show n ++ ")"
 msgShow (MsgFloat64 n) = "Float64(" ++ show n ++ ")"
 msgShow (MsgBin8 xs) = "Bin8(@[" ++ (intercalate "," $ map (\x -> "cast[b8](" ++ show x ++ ")") xs) ++ "])"
@@ -70,6 +70,12 @@ randBinSeq n = sequence [choose (0, 255) | _ <- [1..n]]
 randMsg :: Int -> Gen [Msg]
 randMsg n = sequence [arbitrary | _ <- [1..n]]
 
+randMap :: Int -> Gen [(Msg, Msg)]
+randMap n = do
+    xs <- randMsg n
+    ys <- randMsg n
+    return $ zip xs ys
+
 instance Arbitrary Msg where 
   arbitrary = do
     oneof [
@@ -79,6 +85,7 @@ instance Arbitrary Msg where
       , liftM MsgFixArray $ choose (1, 7) >>= randMsg
       , liftM MsgArray16 $ choose (1, 7) >>= randMsg
       , liftM MsgArray32 $ choose (1, 7) >>= randMsg
+      , liftM MsgFixMap $ choose (1, 5) >>= randMap
       , liftM MsgPFixNum $ choose (0, (1 `shiftL` 7)-1)
       , liftM MsgNFixNum $ choose (0, (1 `shiftL` 5)-1)
       , liftM MsgU8 $ choose (0, (1 `shiftL` 8)-1)
@@ -96,5 +103,5 @@ instance Arbitrary Msg where
       , liftM MsgBin32 $ choose (0, 10) >>= randBinSeq
           ]
 main = do
-  msges <- sequence $ [generate (arbitrary :: Gen Msg) | _ <- [1..1000]] :: IO [Msg]
+  msges <- sequence $ [generate (arbitrary :: Gen Msg) | _ <- [1..100]] :: IO [Msg]
   forM_ msges (\msg -> print $ msg)
