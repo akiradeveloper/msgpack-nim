7ba83aae7a2312f689d85c800240020426991014
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-05 16:59:52 +0900

    update rpc
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- rpc.nim
+++ rpc.nim
@@ -9,15 +9,16 @@ import asyncnet
 import rawsockets
 import streams
 
-proc `$>`[A, B](fut: Future[A], f: proc(x: A): B): Future[B] =
-  let retfut = newFuture[B]("asyncdispatch.`lift`")
-  fut.callback =
-    proc (fut: Future[A]) =
-      if fut.failed:
-        retfut.fail(fut.error)
-      else:
-        retfut.complete(f(fut.read))
-  return retfut
+# proc `$>`[A, B](fut: Future[A], f: proc(x: A): B): Future[B] =
+#   # experimental. use await?
+#   let retfut = newFuture[B]("asyncdispatch.`lift`")
+#   fut.callback =
+#     proc (fut: Future[A]) =
+#       if fut.failed:
+#         retfut.fail(fut.error)
+#       else:
+#         retfut.complete(f(fut.read))
+#   return retfut
 
 type Server = object
   sock: AsyncSocket
@@ -25,41 +26,79 @@ type Server = object
 proc mkServer(sock: AsyncSocket): Server =
   Server(sock: sock)
 
-proc addProc(server: Server, key: string, f: proc (x: seq[Msg]): Msg) =
+proc addProc(server: Server, key: Msg, f: proc (x: seq[Msg]): Msg) =
   discard
 
-proc addNotify(server: Server, key: string, f: proc (x: seq[Msg]): void) =
+proc runProc(server: Server, key: Msg, params: seq[Msg]): Msg =
   discard
+
+proc addNotify(server: Server, key: Msg, f: proc (x: seq[Msg]): void) =
+  discard
+
+proc runNotify(server: Server, key: Msg, params: seq[Msg]) =
+  discard
+
+proc toInt(m: Msg): int = discard
+
+proc doHandle(data: string): seq[Msg] =
+  let st = newStringStream(data)
+  let msg = st.unpack
+  let arr: seq[Msg] = case msg.kind:
+    of mkFixArray:
+      msg.vFixArray
+    of mkArray16:
+      msg.vArray16
+    of mkArray32:
+      msg.vArray32
+    else:
+      @[]
 
 proc handle(server: Server, conn: AsyncSocket) {.async.} =
-  let data = conn.recv(1 shl 60)
-  result = data $> proc (x:string): auto =
-    let st = newStringStream(x)
-    let msg = st.unpack
-    let arr: seq[Msg] = case msg.kind:
-      of mkFixArray:
-        msg.vFixArray
-      of mkArray16:
-        msg.vArray16
-      of mkArray32:
-        msg.vArray32
-      else:
-        @[]
-    arr
-  
+  let data = await conn.recv(1 shl 60)
+  let arr = doHandle(data)
+  case toInt(arr[0]):
+  of 0:
+    let id = toInt(arr[1])
+    let key = arr[2]
+    let r = server.runProc(key, arr[3..arr.high])
+    let res = "aaa"
+    await conn.send(res)
+  of 2:
+    let key = arr[1]
+    server.runNotify(key, arr[2..arr.high])
+  else:
+    assert(false)
+
 proc start(server: Server) {.async.} =
   while true:
     let conn: AsyncSocket = await server.sock.accept
     asyncCheck server.handle(conn)
 
+type ClientGen = object
+
 type Client = object
   sock: AsyncSocket
 
-proc mkClient(sock: AsyncSocket): Client =
+proc mkClient(gen: ClientGen, sock: AsyncSocket): Client =
   Client(sock: sock)
 
 proc call(cli: Client, fun: Msg, params: openArray[Msg]): Future[Msg] {.async.} =
-  discard
+  let retfut: Future[Msg] = newFuture[Msg]()
+  let req = "aaa" # TODO
+  await cli.sock.send(req)
+  let data = await cli.sock.recv(100000)
+  let arr: seq[Msg] = doHandle(data)
+  case toInt(arr[0]):
+  of 1:
+    let id = toInt(arr[1])
+    let success = arr[2].kind == mkNil
+    if success:
+      let r: Msg = arr[3]
+      retfut.complete(r)
+    else:
+      retfut.fail(nil) # TODO
+  else:
+    assert(false)
 
 proc notify(cli: Client, fun: Msg, params: openArray[Msg]): Future[void] {.async.} =
   discard
