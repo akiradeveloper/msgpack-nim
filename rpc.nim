71dc7c0fd919c8229934f0af5a08db6689c03ce9
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-05 11:27:07 +0900

    update rpc
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- rpc.nim
+++ rpc.nim
@@ -15,8 +15,27 @@ type Server = object
 proc mkServer(sock: AsyncSocket): Server =
   Server(sock: sock)
 
-proc start(server: Server) {.async.} =
+proc addProc(server: Server) =
   discard
+
+proc addNotify(server: Server) =
+  discard
+
+proc handle(data: string) =
+  let st = newStringStream(data)
+  let msg = st.unpack
+  let arr: seq[Msg] = case msg.kind:
+    of mkFixArray:
+
+    of mkArray16:
+    of mkArray32:
+
+
+proc start(server: Server) {.async.} =
+  while true:
+    let conn: AsyncSocket = await sock.accept
+    let data = await conn.recv(1 shl 60)
+    server.handle(data)
 
 type Client = object
   sock: AsyncSocket
