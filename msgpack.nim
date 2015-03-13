f73cd8b58c28b267d7b5d4eb9a726d8649669bb5
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-13 21:43:12 +0900

    merge wrappable to API
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- msgpack.nim
+++ msgpack.nim
@@ -233,7 +233,9 @@ proc Ext32*(v: Ext): Msg =
 
 # ------------------------------------------------------------------------------
 
-# Nim Object <=> Msg Object Conversion
+#
+# Nim Object => Msg Object
+#
 
 proc toMsg*(x: bool): Msg =
   assert(false)
@@ -339,6 +341,43 @@ proc toMsg*(x: Ext): Msg =
     Ext16(x)
   else: # FIXME
     Ext32(x)
+
+#
+# Wrappable Type Class
+#
+
+type Wrappable = generic x
+  let x: Msg = wrap(x)
+
+proc wrap*(x: bool): Msg =
+  toMsg(x)
+
+proc wrap*(x: int): Msg =
+  toMsg(x)
+
+proc wrap*(x: float): Msg =
+  toMsg(x)
+
+proc wrap*(x: string): Msg =
+  toMsg(x)
+
+proc wrap*(x: seq[byte]): Msg =
+  toMsg(x)
+
+proc wrap*(x: Ext): Msg =
+  toMsg(x)
+
+proc wrap*[T:Wrappable](x: seq[T]): Msg =
+  x.map(wrap).toMsg
+
+proc wrap*[K: Wrappable, V: Wrappable](x: seq[tuple[key: K, val: V]]): Msg =
+  x.map(proc (e: tuple[key: K, val: V]): auto = (wrap(e.key), wrap(e.val))).toMsg
+
+# ------------------------------------------------------------------------------
+
+#
+# Msg Object => Nim Object
+#
 
 proc unwrapInt*(x: Msg): int =
   case x.kind:
@@ -986,12 +1025,6 @@ proc unpack*(st: Stream): Msg =
 
 # ------------------------------------------------------------------------------
 
-#
-# MessagePack-RPC
-#
-
-# ------------------------------------------------------------------------------
-
 proc t*(msg: Msg) =
   ## Test by cyclic translation. Don't use.
   let before = $expr(msg)
@@ -1046,3 +1079,12 @@ when isMainModule:
 
   t(FixArray(@[FixArray(@[True, False]), PFixNum(18)]))
   # t(FixArray(toseq(@[FixArray(toseq(@[True, False])), FixArray(toseq(@[True, False]))])))
+
+  let x:Msg = wrap(@[1,2])
+  echo x
+
+  let y:Msg = wrap(@[(1,2), (3,4)])
+  echo y
+
+  let z: Msg = wrap(@[@[1], @[2]])
+  echo z
--- wrappable.nim
+++ wrappable.nim
@@ -2,43 +2,10 @@
 
 import msgpack
 
-type Wrappable = generic x
-  let x: Msg = wrap(x)
-
-proc wrap(x: bool): Msg =
-  toMsg(x)
-
-proc wrap(x: int): Msg =
-  toMsg(x)
-
-proc wrap(x: float): Msg =
-  toMsg(x)
-
-proc wrap(x: string): Msg =
-  toMsg(x)
-
-proc wrap(x: seq[byte]): Msg =
-  toMsg(x)
-
-proc wrap(x: Ext): Msg =
-  toMsg(x)
-
-proc wrap[T:Wrappable](x: seq[T]): Msg =
-  x.map(wrap).toMsg
-
-proc wrap[K: Wrappable, V: Wrappable](x: seq[tuple[key: K, val: V]]): Msg =
-  x.map(proc (e: tuple[key: K, val: V]): auto = (wrap(e.key), wrap(e.val))).toMsg
 
 # doesn't compile
 # converter toMsg[T:Wrappable](x: T): Msg =
 #   wrap(x)
 
 when isMainModule:
-  let x:Msg = wrap(@[1,2])
-  echo x
 
-  let y:Msg = wrap(@[(1,2), (3,4)])
-  echo y
-
-  let z: Msg = wrap(@[@[1], @[2]])
-  echo z
